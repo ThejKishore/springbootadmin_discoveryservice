ce1fbd21384d79d8456f485763b4672e68650e48
Thej Kishore <kishores1984@gmail.com>
2017-10-27 00:24:19 -0500

    first commit

---
+++ src/test/java/com/kish/demobootadmin/DemobootadminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kish.demobootadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemobootadminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
